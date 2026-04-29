e1b37e1f3b5ea950abbe7b8ccceede7a56e16c25
simonbrobson <simonbrobson@yahoo.com>
2026-04-28 22:27:16 -0500

    Add Work Orders page

---
+++ app/work-orders/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function WorkOrders() {
+  return (
+    <div style={{ padding: 30 }}>
+      <h1>📋 Work Orders</h1>
+      <p>Total Work Orders: 0</p>
+      <p>Open: 0</p>
+      <p>Completed: 0</p>
+    </div>
+  );
+}
